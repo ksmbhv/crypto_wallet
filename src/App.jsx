c860c9f0f45e6cb8fae79768ea115830c6e5b5d9
ksmbhv <ksupadhyaya27@gmail.com>
2025-03-05 00:15:59 +0530

    App.jsx added, still have to add SolWallet & Eth Wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import { generateMnemonic } from "bip39";
+import { SolanaWallet } from './SolanaWallet';
+import { EthWallet } from './EthWallet';
+
 function App() {
-  <br>
-    Hi world  
-  </br>
+  const [mnemonic, setMnemonic] = useState("");
+
+  return (
+    <>
+      <input type="text" value={mnemonic}></input>
+      <button onClick={async function() {
+        const mn = generateMnemonic();
+        setMnemonic(mn)
+      }}>
+        Create Seed Phrase
+      </button>
+
+      {mnemonic && <SolanaWallet mnemonic={mnemonic} />}
+      {mnemonic && <EthWallet mnemonic={mnemonic} />}
+    </>
+  )
 }
+
+export default App
